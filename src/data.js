0d064dd64838057211202cbe8c5764a87ca2ee9b
auytsgfa <plmaregigi@gmail.com>
2025-07-27 02:22:30 +0300

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,21 @@
+module.exports = {
+  preturiArme: {
+    Tec9: 250000,
+    TecPistol: 350000,
+    AdvancedRifle: 500000,
+    AK47: 400000,
+    PDW: 350000,
+  },
+
+  prafPerGlont: {
+    Tec9: 2.25,
+    TecPistol: 2.7,
+    AdvancedRifle: 4.5,
+    AK47: 3.75,
+    PDW: 2.7,
+  },
+
+  weaponList: ["Tec9", "TecPistol", "AdvancedRifle", "AK47", "PDW"],
+
+  bulletList: ["Tec9", "TecPistol", "AdvancedRifle", "AK47", "PDW"],
+};
